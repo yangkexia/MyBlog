6c2ecae9c074808d7589188bdfaebf8ef46160b0
yangkexia <972505248@qq.com>
2019-02-01 17:38:45 +0800

    新增Home.vue及样式文件

---
+++ src/components/home.vue
@@ -0,0 +1,25 @@
+<template>
+	<div id="home">
+		<div class="header">header</div>
+		<div class="content">
+			<div class="l_box">文章列表</div>
+			<div class="r_box">文章内容</div>
+		</div>
+		<div class="footer">footer</div>
+	</div>
+</template>
+
+<script>
+	export default {
+		name: 'home',
+		data() {
+			return {
+				
+			}
+		}
+	}
+</script>
+
+<style scoped lang="less">
+	@import url("../assets/css/home.less");
+</style>
